c71cda26388e1878278cbc12e1acf9f10c4a9c40
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2021-07-22 19:46:44 +0200

    Change account key

--- interact.go
+++ interact.go
@@ -15,7 +15,7 @@ import (
 	"github.com/yannvon/aposteriori/contracts"
 )
 
-const Key1 = ***REMOVED***
+const Key1 = ``
 const Key2 = `{"address":"8f2fefcec9c4990beecbc024fb766f1c4c0fba13","crypto":{"cipher":"aes-128-ctr","ciphertext":"306ab023028a65eaaf0d22567854e46a578c136db2b079b4f3ef16b79a8fa9fd","cipherparams":{"iv":"ec4e1202d7f961365071b586eb01c68d"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"868c85b762aa0c29fcec8693017c421e9d78a68282db7247aad1213f37fafa9c"},"mac":"9ba6185bbacd8de6b6b0e0e476eb8189f4315961920c4adee678010e177fcf16"},"id":"34f829a9-b680-42cf-9d85-01f1923294b6","version":3}`
 const Key3 = `{"address":"28e7caf4e28206df61cb1b944ca605161fcd5748","crypto":{"cipher":"aes-128-ctr","ciphertext":"30a69d8de0e2d3e3dd41c94b7115b5e2da6c67d0c5f901f2818cf8ecdf526d83","cipherparams":{"iv":"8549263bed0a8260e9315db68b3583fc"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"9a1fbba59f1dc83dc8f695055cc60b789331d3afec0a1c78666e005ec76e2537"},"mac":"2ada6c4f978f1502945c9b16074c2135a7d0a7c4eb28488ff0dc72466429bdf0"},"id":"0adf3199-8608-442a-8252-ad449d021554","version":3}`
 const Key4 = `{"address":"7e8dcd2e30485fd4db1f7c6fa1c4fc875a45f160","crypto":{"cipher":"aes-128-ctr","ciphertext":"955e0f9a35656359f86c218d87f4d19a80a85c8e1f6d337559018efe02e91565","cipherparams":{"iv":"1440936dd6f1fd4027a1d1ed9cf1519d"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"3b435081a54872e804b4538af96a89aed7962dbd6b06a01f7f14022d2294eabb"},"mac":"969d47895d3bed9352fab33360bf7dee48fc867be3ec445c1e6b5c93949c8d28"},"id":"8996a081-6d52-4cd5-8756-bae51b69c4ed","version":3}`
