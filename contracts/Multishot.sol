3927b9f7cb18c6d1d1072792675959d0aa9086b0
Yann Vonlanthen <yann.vonlanthen@epfl.ch>
2021-08-13 19:19:47 +0200

    Clean up contract

--- contracts/Multishot.sol
+++ contracts/Multishot.sol
@@ -27,25 +27,22 @@ contract Multishot {
     
     
     /** 
-     * @dev Create a new multishot.
+     * @dev Create a new multishot contract
      * @param validators addresses allowed to propose tx hash value
      * @param stake corresponding stake
      */
     constructor(address[] memory validators, uint[] memory stake) {
-        
-        uint tmp = 0;
 
+        uint tmp = 0;
         for (uint i = 0; i < validators.length; i++) {
-            
             validatorStake[validators[i]] = stake[i];
             tmp += stake[i];
         }
-        
         majorityStake = (uint(4) * tmp) / uint(5);
     }
     
     /** 
-     * @dev 
+     * @dev Access current decision status for consensus instnce
      * @param txOrigin public address of tx sender
      * @param nonce nonce of tx
      */
@@ -54,7 +51,7 @@ contract Multishot {
         
         Decision storage dec = decisions[txOrigin][nonce];
         
-        // Check if decision created
+        // Check if decision reached
         if (dec.decision != 0) {
             return dec.decision;
         } else {
@@ -64,42 +61,35 @@ contract Multishot {
 
 
     /**
-     * @dev 
+     * @dev Function to propose value to given concensus instance
+     * @param txOrigin public address of tx sender
+     * @param nonce nonce of tx
      */
     function propose(address txOrigin, uint nonce, uint txHash) public {
 
         Decision storage dec = decisions[txOrigin][nonce];
 
-        // Create new decision if necessary
-        //if (dec == 0) {
-        //    dec = Decision({}); // Create new decision struct, FIXME needed ?
-        //}
-
         require(!dec.proposed[msg.sender], "You already proposed.");
-        require(dec.decision == 0, "Consensus already delivered.");
+        require(dec.decision == 0, "Instance already decided.");
         require(validatorStake[msg.sender] > 0, "You need stake to be able to propose a value.");
 
-
+        // Tally proposal
         dec.total_ack_weight[txHash] += validatorStake[msg.sender]; 
         dec.weight_received += validatorStake[msg.sender];
 
-        // Adapt currentMax if necessary
+        // Adapt current max if necessary
         if (dec.total_ack_weight[dec.currentMax] < dec.total_ack_weight[txHash]) {
             dec.currentMax = txHash;
         }
 
         // Check if we can decide on value
-        // Decision can be reached for a DoubleSpend with following constraints
-        // 1) more than 4/5 of stake received
-        // 2) decide on max value
-
+        // Decision can be reached if more than 4/5 of stake received
         if (dec.weight_received > majorityStake) {
-            // Decision can be made
             dec.decision = dec.currentMax;
 
             // Emit event, such that processes don't have to query themselves
             emit Decided(txOrigin, nonce, dec.decision);
-        }      
+        } 
 
         // Save new decision object & mark sender as having already proposed
         dec.proposed[msg.sender] = true;
